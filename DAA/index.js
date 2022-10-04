b75a392f5d972ffa07626e350b83ff588fb11820
Reiaslite <raidearaujo06@gmail.com>
2022-10-04 21:01:09 +0700

    partial update

--- DAA/index.js
+++ DAA/index.js
@@ -1,9 +1,32 @@
-function a(b) {
-    if (b === 0) {
+REKURSIF
+function f(n) {
+    if (n === 0) {
         return 0;
     } else {
-        return b + a (--b);
+        return n + f (--n);
     }
 }
 
-console.log(a(4));
+console.log(f(4));
+
+FAKTORIAL REKURSIF
+function f(n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return (n * f(n-1))
+    }
+}
+
+console.log(f(5));
+
+FIBONNACI REKURSIF
+function fib(n) {
+    if (n < 3) {
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+console.log(fib(20));
