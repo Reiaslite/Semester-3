24338a6afdbd30f9173e1d486975480807d03129
Reiaslite <raidearaujo06@gmail.com>
2022-10-27 18:15:24 -0700

    partial update

--- DAA/Tugas-1/l3.js
+++ DAA/Tugas-1/l3.js
@@ -1,9 +1,9 @@
-console.time('Execution time');
+console.time("Execution time");
 
 for (let i = 1; i <= 1000; i++) {
-    console.log(i);
+  console.log(i);
 }
 
-console.timeEnd('Execution time');
+console.timeEnd("Execution time");
 const used = process.memoryUsage().heapUsed / 1024 / 1024;
 console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
